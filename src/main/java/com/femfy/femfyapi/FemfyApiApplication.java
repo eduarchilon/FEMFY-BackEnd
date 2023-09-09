c141373dbed1299b74b63883209074d1e9538327
eduarchilon <eduarchilon@outlook.es>
2023-09-09 16:49:10 -0300

    first version

---
+++ src/main/java/com/femfy/femfyapi/FemfyApiApplication.java
@@ -0,0 +1,13 @@
+package com.femfy.femfyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FemfyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FemfyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/femfy/femfyapi/FemfyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femfy.femfyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FemfyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
