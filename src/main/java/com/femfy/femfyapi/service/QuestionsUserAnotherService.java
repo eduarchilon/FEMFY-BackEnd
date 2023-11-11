2bb58514c7e919b5a18b7794170fec22186034d9
walter <walter.guglielmettigmail.com>
2023-11-10 22:27:39 -0300

    Se agrega mapeo de campos para el update y la respuesta

--- src/main/java/com/femfy/femfyapi/service/QuestionsUserAnotherService.java
+++ src/main/java/com/femfy/femfyapi/service/QuestionsUserAnotherService.java
@@ -90,14 +90,34 @@ public class QuestionsUserAnotherService implements IQuestionsUserAnotherService
         }
 
         if(questionsUserAnother.getCongenitalCauses() != null){
-            QuestionsUserAnotherCongenitalCausesDTO congenitalCausesDTO = new QuestionsUserAnotherCongenitalCausesDTO();
-            congenitalCausesDTO.setId(questionsUserAnother.getCongenitalCauses().getId());
+        	QuestionsUserAnotherCongenitalCausesDTO congenitalCausesDTO = new QuestionsUserAnotherCongenitalCausesDTO();
+        	if(questionsUserAnother.getCongenitalCauses().getId()!=null) {
+        		congenitalCausesDTO.setId(questionsUserAnother.getCongenitalCauses().getId());
+                congenitalCausesDTO.setAnother(questionsUserAnother.getCongenitalCauses().getAnother());
+                congenitalCausesDTO.setAnotherDescription(questionsUserAnother.getCongenitalCauses().getAnotherDescription());
+                congenitalCausesDTO.setMalformationsUterine(questionsUserAnother.getCongenitalCauses().getMalformationsUterine());
+                congenitalCausesDTO.setTurnerSyndrome(questionsUserAnother.getCongenitalCauses().getTurnerSyndrome());
+        	}
+                        
             dto.setCongenitalCauses(congenitalCausesDTO);
         }
 
         if(questionsUserAnother.getHormonalCauses() != null){
             QuestionsUserAnotherHormonalCausesDTO hormonalCausesDTO = new QuestionsUserAnotherHormonalCausesDTO();
-            hormonalCausesDTO.setId(questionsUserAnother.getHormonalCauses().getId());
+            
+            if(questionsUserAnother.getHormonalCauses().getId()!=null){
+            	hormonalCausesDTO.setId(questionsUserAnother.getHormonalCauses().getId());
+            	hormonalCausesDTO.setPolycysticOvarySyndrome(questionsUserAnother.getHormonalCauses().getPolycysticOvarySyndrome());
+            	hormonalCausesDTO.setHypothyroidism(questionsUserAnother.getHormonalCauses().getHypothyroidism());
+            	hormonalCausesDTO.setHyperprolactinemia(questionsUserAnother.getHormonalCauses().getHyperprolactinemia());
+            	hormonalCausesDTO.setSheehanSyndrome(questionsUserAnother.getHormonalCauses().getSheehanSyndrome());
+            	hormonalCausesDTO.setPrematureOvarianFailure(questionsUserAnother.getHormonalCauses().getPrematureOvarianFailure());
+            	hormonalCausesDTO.setHypothalamicDisorders(questionsUserAnother.getHormonalCauses().getHypothalamicDisorders());
+            	hormonalCausesDTO.setInsulinResistance(questionsUserAnother.getHormonalCauses().getInsulinResistance());
+            	hormonalCausesDTO.setAnother(questionsUserAnother.getHormonalCauses().getAnother());
+            	hormonalCausesDTO.setAnotherDescription(questionsUserAnother.getHormonalCauses().getAnotherDescription());
+            }
+            
             dto.setHormonalCauses(hormonalCausesDTO);
         }
 
@@ -135,21 +155,84 @@ public class QuestionsUserAnotherService implements IQuestionsUserAnotherService
     }
 
     private void updateQuestionsUserAnotherFields(QuestionsUserAnother existingQuestionsUserAnother, QuestionsUserAnotherDTO updatedDTO) {
-        if (updatedDTO.getUserId() != null) {
+        
+    	if (updatedDTO.getUserId() != null) {
             User user = new User();
             user.setId(updatedDTO.getUserId());
             existingQuestionsUserAnother.setUser(user);
         }
 
         if(updatedDTO.getHormonalCauses() != null){
+        	
             QuestionsUserAnotherHormonalCauses questionsUserAnotherHormonalCauses = new QuestionsUserAnotherHormonalCauses();
-            questionsUserAnotherHormonalCauses.setId(updatedDTO.getId());
+            
+            if(updatedDTO.getHormonalCauses().getId()!=null) {
+            	questionsUserAnotherHormonalCauses.setId(updatedDTO.getHormonalCauses().getId());
+            }
+            
+            if(updatedDTO.getHormonalCauses().getAnother()!= null) {
+            	questionsUserAnotherHormonalCauses.setAnother(updatedDTO.getHormonalCauses().getAnother());
+            }
+            
+            if(updatedDTO.getHormonalCauses().getAnotherDescription()!=null && !updatedDTO.getHormonalCauses().getAnotherDescription().isEmpty()) {
+            	questionsUserAnotherHormonalCauses.setAnotherDescription(updatedDTO.getHormonalCauses().getAnotherDescription());	
+            }
+            
+            if(updatedDTO.getHormonalCauses().getHyperprolactinemia()!=null) {
+            	questionsUserAnotherHormonalCauses.setHyperprolactinemia(updatedDTO.getHormonalCauses().getHyperprolactinemia());
+            }
+
+            if(updatedDTO.getHormonalCauses().getHypothyroidism()!=null) {
+            	questionsUserAnotherHormonalCauses.setHypothyroidism(updatedDTO.getHormonalCauses().getHypothyroidism());
+            }
+            
+            if(updatedDTO.getHormonalCauses().getHypothalamicDisorders()!=null) {
+            	questionsUserAnotherHormonalCauses.setHypothalamicDisorders(updatedDTO.getHormonalCauses().getHypothalamicDisorders());
+            }
+            
+            if(updatedDTO.getHormonalCauses().getPolycysticOvarySyndrome()!=null) {
+            	questionsUserAnotherHormonalCauses.setPolycysticOvarySyndrome(updatedDTO.getHormonalCauses().getPolycysticOvarySyndrome());
+            }
+            
+            if(updatedDTO.getHormonalCauses().getSheehanSyndrome()!=null) {
+            	questionsUserAnotherHormonalCauses.setSheehanSyndrome(updatedDTO.getHormonalCauses().getSheehanSyndrome());
+            }
+            
+            if(updatedDTO.getHormonalCauses().getPrematureOvarianFailure()!=null) {
+            	questionsUserAnotherHormonalCauses.setPrematureOvarianFailure(updatedDTO.getHormonalCauses().getPrematureOvarianFailure());
+            }
+            
+            if(updatedDTO.getHormonalCauses().getInsulinResistance()!=null) {
+            	questionsUserAnotherHormonalCauses.setInsulinResistance(updatedDTO.getHormonalCauses().getInsulinResistance());
+            }
+            
             existingQuestionsUserAnother.setHormonalCauses(questionsUserAnotherHormonalCauses);
+            
         }
 
         if(updatedDTO.getCongenitalCauses() != null){
             QuestionsUserAnotherCongenitalCauses questionsUserAnotherCongenitalCauses = new QuestionsUserAnotherCongenitalCauses();
-            questionsUserAnotherCongenitalCauses.setId(updatedDTO.getId());
+            
+            if(updatedDTO.getCongenitalCauses().getId()!=null) {
+            	questionsUserAnotherCongenitalCauses.setId(updatedDTO.getCongenitalCauses().getId());
+            }
+            
+            if(updatedDTO.getCongenitalCauses().getMalformationsUterine()!=null) {
+            	questionsUserAnotherCongenitalCauses.setMalformationsUterine(updatedDTO.getCongenitalCauses().getMalformationsUterine());
+            }
+            
+            if(updatedDTO.getCongenitalCauses().getTurnerSyndrome()!=null) {
+            	questionsUserAnotherCongenitalCauses.setTurnerSyndrome(updatedDTO.getCongenitalCauses().getTurnerSyndrome());
+            }
+            
+            if(updatedDTO.getCongenitalCauses().getAnother()!= null) {
+            	questionsUserAnotherCongenitalCauses.setAnother(updatedDTO.getCongenitalCauses().getAnother());
+            }
+            
+            if(updatedDTO.getCongenitalCauses().getAnotherDescription()!=null && !updatedDTO.getCongenitalCauses().getAnotherDescription().isEmpty()) {
+            	questionsUserAnotherCongenitalCauses.setAnotherDescription(updatedDTO.getCongenitalCauses().getAnotherDescription());	
+            }
+            
             existingQuestionsUserAnother.setCongenitalCauses(questionsUserAnotherCongenitalCauses);
         }
 
@@ -159,6 +242,10 @@ public class QuestionsUserAnotherService implements IQuestionsUserAnotherService
 
         if (updatedDTO.getAnotherDescription() != null) {
             existingQuestionsUserAnother.setAnotherDescription(updatedDTO.getAnotherDescription());
+        }
+        
+        if(updatedDTO.getId()!=null) {
+        	existingQuestionsUserAnother.setId(updatedDTO.getId());
         }
     }
 }
