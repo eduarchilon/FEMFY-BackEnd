7cba6bcb98445041b7f775c5550e7e86562a378a
AramiAquino <jaquinoromero@alumno.unlam.edu.ar>
2023-11-11 12:04:55 -0300

    getQuestionsMenopauseByUser

--- src/main/java/com/femfy/femfyapi/repository/QuestionsUserMenopauseRepository.java
+++ src/main/java/com/femfy/femfyapi/repository/QuestionsUserMenopauseRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.femfy.femfyapi.entity.QuestionsUserMenopause;
 
+import  java.util.List;
+
 @Repository
 public interface QuestionsUserMenopauseRepository extends JpaRepository<QuestionsUserMenopause, Long> {
-
+    List<QuestionsUserMenopause> findByUserId(Long userId);
 }
